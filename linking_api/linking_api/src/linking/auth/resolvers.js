e4fc4efcc914cfeb45a89b56fe310c971114dc65
aeromeror <56206150+aeromeror@users.noreply.github.com>
2021-12-06 21:58:31 -0500

    Add files via upload

---
+++ linking_api/linking_api/src/linking/auth/resolvers.js
@@ -0,0 +1,25 @@
+import { generalRequest, getRequest } from '../../utilities';
+import { url, port, entryPoint } from './server';
+
+const URL = `http://${url}:${port}/${entryPoint}`;
+
+const resolvers = {
+	Query: {
+		profile:(_, { token })=>
+			generalRequest(`${URL}/profile`, 'GET',token,{'authtoken' : token.token}),
+		uservalidation:(_, { token })=>
+		generalRequest(`${URL}/confirm/${token.token}`, 'GET'),
+		image:(_, { image })=>
+		generalRequest(`${URL}/image/`, 'GET',image),
+	},
+	Mutation: {
+		signup:(_, { user }) =>
+			generalRequest(`${URL}/signup`, 'POST', user),
+		signin:(_, { user }) =>
+			generalRequest(`${URL}/signin`, 'POST', user),
+		profile:(_, { user }) =>
+			generalRequest(`${URL}/profile`, 'PUT', user,{'authtoken' : user.token}),
+	}
+};
+
+export default resolvers;
---
+++ linking_api/linking_api/src/linking/auth/typeDefs.js
@@ -0,0 +1,47 @@
+export const categoryTypeDef = `
+  type User {
+    username: String!
+    email:  String!
+    password: String!
+    image: String
+    status: String
+  }
+  type token {
+    authtoken: String!
+  }
+  type Image {
+    image: String!
+  }
+  input UserInput {
+     username: String!
+     email:  String!
+     password: String!
+  }
+  input LoginInput {
+    email:  String!
+    password:String!
+  }input Usertoken {
+    token: String!
+  }
+  input UserupdInput {
+  username: String
+  password: String
+  image: String
+  token: String!
+  }
+  input Imageinput {
+    image: String!
+  }
+  `;
+
+export const categoryQueries = `
+      profile(token:Usertoken!):User!
+      uservalidation(token:Usertoken!):User!
+      image(image:Imageinput!):Image!
+  `;
+
+export const categoryMutations = `
+    signup(user: UserInput!): User!
+    signin(user: LoginInput!): token!
+    profile(user:UserupdInput): User!
+`;
