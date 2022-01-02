fb47cc367077736d10ba3c3982fb1d88859ee645
Diego <dfsanchezme@gmail.com>
2022-01-02 18:46:10 -0500

    Changing user_id to user_email in settings_ms

--- linking_api/linking_api/src/linking/settings/typeDefs.js
+++ linking_api/linking_api/src/linking/settings/typeDefs.js
@@ -1,34 +1,34 @@
 export const configTypeDef = `
   type Configuration {
       config_id: Int!
-      user_id: Int!      
+      user_email: String!      
       subtitles: Boolean!
       background_id: Int
-      in_device: String!
-      out_device: String!
+      in_device: String
+      out_device: String
   }
   input newConfig {
-      user_id: Int!      
+      user_email: String!      
       subtitles: Boolean!
-      in_device: String!
-      out_device: String!
+      in_device: String
+      out_device: String
   }
   input updateConfig {
     config_id: Int!      
     subtitles: Boolean!
-    in_device: String!
-    out_device: String!
+    in_device: String
+    out_device: String
   }
 `;
 
 export const configQueries = `
       allConfig: [Configuration]!
-      userConfig(user_id: Int!): Configuration!
+      userConfig(user_email: String!): Configuration!
   `;
 
 export const configMutations = `
     createConfig(configuration: newConfig!): Configuration!
     updateConfig(configuration: updateConfig!): String!
-    updateUserBackground(user_id: Int!, background_id: Int): String!
+    updateUserBackground(user_email: String!, background_id: Int): String!
     deleteConfig(config_id: Int!): String!
 `;
